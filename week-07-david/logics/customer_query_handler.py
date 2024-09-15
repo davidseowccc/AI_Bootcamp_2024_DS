ca5d7b3a1888ca191ab74f60e8ca9fafaac6e538
davidseowccc <62555082+davidseowccc@users.noreply.github.com>
2024-09-15 23:10:35 +0800

    Update customer_query_handler.py

--- week-07-david/logics/customer_query_handler.py
+++ week-07-david/logics/customer_query_handler.py
@@ -23,11 +23,24 @@ category_n_course_name = {'Programming and Development': ['Web Development Bootc
                           'Design': ['Graphic Design Essentials', 'UI/UX Design Fundamentals']}
 
 # Load the JSON file
-filepath = './data/courses-full.json'
-with open(filepath, 'r') as file:
-    json_string = file.read()
-    dict_of_courses = json.loads(json_string)
+# filepath = './data/courses-full.json'
+# with open(filepath, 'r') as file:
+#     json_string = file.read()
+#     dict_of_courses = json.loads(json_string)
 
+
+import json
+import requests
+
+# URL of the raw JSON file on GitHub
+# url = 'https://raw.githubusercontent.com/your-username/your-repository/main/data/courses-full.json'
+# url = 'https://github.com/davidseowccc/AI_Bootcamp_2024_DS/blob/main/week-07-david/data/courses-full.json'
+url = 'https://raw.githubusercontent.com/AI_Bootcamp_2024_DS/week-07-david/data/courses-full.json'
+
+# Fetch the JSON file from GitHub
+response = requests.get(url)
+json_string = response.text
+dict_of_courses = json.loads(json_string)
 
 def identify_category_and_courses(user_message):
     delimiter = "####"
