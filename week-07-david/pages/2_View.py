be38fb976a586c9af7506af6cd8a65c2c0299623
davidseowccc <62555082+davidseowccc@users.noreply.github.com>
2024-09-15 23:27:11 +0800

    Update 2_View.py

--- week-07-david/pages/2_View.py
+++ week-07-david/pages/2_View.py
@@ -2,11 +2,23 @@ import os
 import json
 import pandas as pd
 
-# Load the JSON file
-filepath = './data/courses-full.json'
-with open(filepath, 'r') as file:
-    json_string = file.read()
-    dict_of_courses = json.loads(json_string)
+# # Load the JSON file
+# filepath = './data/courses-full.json'
+# with open(filepath, 'r') as file:
+#     json_string = file.read()
+#     dict_of_courses = json.loads(json_string)
+
+import json
+import requests
+
+# URL of the raw JSON file on GitHub
+# url = 'https://raw.githubusercontent.com/your-username/your-repository/main/data/courses-full.json'
+# url = 'https://github.com/davidseowccc/AI_Bootcamp_2024_DS/blob/main/week-07-david/data/courses-full.json'
+url = 'https://raw.githubusercontent.com/AI_Bootcamp_2024_DS/week-07-david/data/courses-full.json'
+
+# Fetch the JSON file from GitHub
+response = requests.get(url)
+dict_of_courses = response.text
 
 # Create the DataFrame and Transpose Table
 df = pd.DataFrame(dict_of_courses).T
