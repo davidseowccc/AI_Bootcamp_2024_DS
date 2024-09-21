0e1a47c121f7dbeaa75e41f21b37bd153bc0c786
davidseowccc <62555082+davidseowccc@users.noreply.github.com>
2024-09-22 00:12:06 +0800

    Add files via upload

---
+++ week-07-david/helper_functions/utility.py
@@ -0,0 +1,32 @@
+# filename: utility.py
+# https://docs.streamlit.io/knowledge-base/deploy/authentication-without-sso
+
+import streamlit as st  
+import random  
+import hmac  
+  
+# """  
+# This file contains the common components used in the Streamlit App.  
+# This includes the sidebar, the title, the footer, and the password check.  
+# """  
+  
+  
+def check_password():  
+    """Returns `True` if the user had the correct password."""  
+    def password_entered():  
+        """Checks whether a password entered by the user is correct."""  
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):  
+            st.session_state["password_correct"] = True  
+            del st.session_state["password"]  # Don't store the password.  
+        else:  
+            st.session_state["password_correct"] = False  
+    # Return True if the passward is validated.  
+    if st.session_state.get("password_correct", False):  
+        return True  
+    # Show input for password.  
+    st.text_input(  
+        "Password", type="password", on_change=password_entered, key="password"  
+    )  
+    if "password_correct" in st.session_state:  
+        st.error("😕 Password incorrect")  
+    return False
