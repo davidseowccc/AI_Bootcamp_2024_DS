143aaad542c2173addddb3a996cfc5f56e6bd652
davidseowccc <62555082+davidseowccc@users.noreply.github.com>
2024-09-22 16:54:36 +0800

    Update Start.py

--- week-07-david/Start.py
+++ week-07-david/Start.py
@@ -9,16 +9,43 @@ from helper_functions.utility import check_password
 
 #####
 # Demo password or no password required
-while True:
-    pw = st.text_input('Select "1" to BYPASS password.')
+# while True:
+#     pw = st.text_input('Select "1" to BYPASS password.')
     
-    if (pw == "1"):
-        break
+#     if (pw == "1"):
+#         break
 
+#     else:
+#         # Check if the password is correct.  
+#         if check_password() == False:  
+#             st.stop()
+
+# Set an initial state for bypass if it doesn't exist yet
+if 'bypass' not in st.session_state:
+    st.session_state.bypass = False
+
+# If the bypass is not activated, show the password input
+if not st.session_state.bypass:
+    pw = st.text_input('Enter "1" to BYPASS the password.')
+
+    # Check if the user enters "1" to bypass the password check
+    if pw == "1":
+        st.session_state.bypass = True
+        st.success("Password bypassed!")
+    
     else:
-        # Check if the password is correct.  
-        if check_password() == False:  
-            st.stop()
+        # Here you can check the password using your `check_password()` function
+        if st.button("Submit Password"):
+            if check_password() == False:
+                st.error("Incorrect password. Please try again.")
+                st.stop()
+            else:
+                st.success("Correct password!")
+
+# If bypass is activated, proceed without asking for a password
+else:
+    st.write("Password bypassed! Proceeding without password.")
+    # Your application logic goes here
 
 #####
 
